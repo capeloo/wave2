5e85bf48fb307939393c7f7f455b1803b7128cb2
Caio Henrique Capêlo <caiocapelo@alu.ufc.br>
2024-09-30 11:21:13 -0300

    Criação de alguns keyEvents
    - class KeyInput
      - keyPressed( )
      - keyReleased( )

--- src/com/tutorial/main/Game.java
+++ src/com/tutorial/main/Game.java
@@ -18,12 +18,13 @@ public class Game extends Canvas implements Runnable{
 	private Handler handler;
 	
 	public Game() {
+		handler = new Handler();
+		this.addKeyListener(new KeyInput(handler));
+		
 		new Window(WIDTH, HEIGHT, "Wave2", this);
 		
-		handler = new Handler();
-		
-		handler.addObject(new Player(100, 100, ID.Player));
-		handler.addObject(new Player(200, 200, ID.Player));
+		handler.addObject(new Player(WIDTH/2-32, HEIGHT/2-32, ID.Player));
+		handler.addObject(new Player(WIDTH/2+64, HEIGHT/2-32, ID.Enemy));
 	}
 
 	public synchronized void start() {
@@ -84,7 +85,7 @@ public class Game extends Canvas implements Runnable{
 			
 			if(System.currentTimeMillis() - timer > 1000) {
 				timer += 1000;
-				System.out.println("FPS: " + frames);
+				//System.out.println("FPS: " + frames);
 				frames = 0;
 			}
 		}
--- src/com/tutorial/main/Player.java
+++ src/com/tutorial/main/Player.java
@@ -7,8 +7,6 @@ public class Player extends GameObject {
 
 	public Player(int x, int y, ID id) {
 		super(x, y, id);
-		
-		velX = 1;
 	}
 
 	@Override
@@ -19,8 +17,9 @@ public class Player extends GameObject {
 
 	@Override
 	public void render(Graphics g) {
-		g.setColor(Color.white);
+		if(id == ID.Player) g.setColor(Color.white);
+		else if(id == ID.Enemy) g.setColor(Color.red);
 		g.fillRect(x, y, 32, 32);
-	}
+	} 
 
 }
